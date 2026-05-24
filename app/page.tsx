71d0633574add26bde716aae8ca77f4e595199c0
waroon <waroon01@gmail.com>
2026-05-24 19:29:24 +0700

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-      Hello World
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-center text-6xl font-bold">green & gram linebot api webhook</div>
     </div>
   );
 }
